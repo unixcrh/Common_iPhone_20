38f4ff5af2a8d4633917179137ad87a3fe4ed92e
xiaotaowang <445242970@qq.com>
2012-06-08 10:51:58 +0800

    support pull up to reload more.

---
+++ Core/Core-Components-View/EGOTableViewPullRefresh_old/Classes/View/EGORefreshTableViewConstant.h
@@ -0,0 +1,14 @@
+//
+//  EGORefreshTableViewConstant.h
+//  Travel
+//
+//  Created by 小涛 王 on 12-6-7.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#ifndef Travel_EGORefreshTableViewConstant_h
+#define Travel_EGORefreshTableViewConstant_h
+
+
+
+#endif
---
+++ Core/Core-Components-View/EGOTableViewPullRefresh_old/Classes/View/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Travel
+//
+//  Created by 小涛 王 on 12-6-7.
+//  Copyright (c) 2012年 甘橙软件. All rights reserved.
+//
+
+#ifndef Travel_Header_h
+#define Travel_Header_h
+
+
+
+#endif
