02b44f8de66a3bf652d9263a25fc0ffab25e6987
GamyHuang <hguangm2009@gmail.com>
2012-07-03 15:34:26 +0800

    add status bar.

---
+++ Core/Core-Components-View/CustomStatueBar/CustomStatueBar.h
@@ -0,0 +1,28 @@
+//
+//  CustomStatueBar.h
+//  CustomStatueBar
+//
+//  Created by 贺 坤 on 12-5-21.
+//  Copyright (c) 2012年 深圳市瑞盈塞富科技有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class CustomStatueBar;
+@protocol CustomStatueBarDelegate <NSObject>
+
+@optional
+- (void)didClickStatueBar:(CustomStatueBar *)statueBar;
+
+@end
+
+@interface CustomStatueBar : UIWindow
+{
+    UIButton *textButton;
+}
+- (void)showStatusMessage:(NSString *)message;
+- (void)hide;
+- (void)changeMessge:(NSString *)message;
+
+@property(nonatomic, assign) id<CustomStatueBarDelegate>customStatueBarDelegate;
+@end
