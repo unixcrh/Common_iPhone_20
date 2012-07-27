082f3681559cec526f49804657d4cfe6b9ec254c
Benson Peng <penglingzhe@gmail.com>
2012-07-27 16:49:25 +0800

    add getBoolValue for MobClickUtils

--- 3PP-SDK/MobClick/MobClickUtils.m
+++ 3PP-SDK/MobClick/MobClickUtils.m
@@ -20,6 +20,17 @@
     return [value intValue];
 }
 
++ (int)getBoolValueByKey:(NSString*)key defaultValue:(BOOL)defaultValue
+{
+    NSString* value = [MobClick getConfigParams:key];
+    if (value == nil || [value length] == 0){
+        return defaultValue;
+    }
+    
+    return ([value intValue] != 0);
+}
+
+
 + (NSString*)getStringValueByKey:(NSString*)key defaultValue:(NSString*)defaultValue
 {
     NSString* value = [MobClick getConfigParams:key];
